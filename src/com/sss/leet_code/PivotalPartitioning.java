2632aac151ff84abb7fe9c76f700b3f986aa1ef2
Shivend S Singh <s.s.singh1992@gmail.com>
2024-05-05 21:53:28 +0530

    Adding Codes

--- src/com/sss/leet_code/PivotalPartitioning.java
+++ src/com/sss/leet_code/PivotalPartitioning.java
@@ -1,73 +1,60 @@
 package com.sss.leet_code;
 
+import java.util.Arrays;
+
 public class PivotalPartitioning {
+    /*
+            1, 4, 5, 9, 2, 6, 8
+            k = 5
+
+            1, 4, 2, 5, 9, 6, 8
+
+     */
+
+
     public static void main(String[] args) {
-        int[] arr = { 1, 5, 2, 3, 4};
-        //pivotalPartitioning(arr, 0, arr.length, 3);
-        partition(arr);
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
+        int[] arr = {1, 4, 5, 9, 2, 6, 8};
+        int n = arr.length;
+        int k = 5;
+        int[] result = soln(arr, n, k);
+        System.out.println(Arrays.toString(result));
     }
 
-    public static void pivotArray(int[] nums, int pivot) {
-        int pivotCount = 0, low = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < pivot) {
-                swapII(nums, low++, i);
-            } else if (nums[i] == pivot) {
-                swapII(nums, low + pivotCount++, i);
-            }
-        }
-    }
-    private static void swapII(int[] nums, int l, int r) {
-        if (l == r) return;
-        int n = nums[r];
-        for (int i = r - 1; i >= l; i--) {
-            nums[i + 1] = nums[i];
-        }
-        nums[l] = n;
-    }
-
-    static int partition(int[] a){
-        int pivot = 4;
-        int temp, index = 0;
-        int last = a.length -1;
-
-        for(int i = 1; i < a.length; i++){
-            if( a[i] <= pivot){
-                temp = a[i];
-                a[i] = pivot;
-                a[i-1] = temp;
-                index = i;
-            } else if( a[i] > pivot && last > i){
-                temp = a[i];
-                a[i] = a[last];
-                a[last] = temp;
-                last -= 1;
-                i--;
-            }
-            else
+    private static int[] soln(int[] arr, int n, int k) {
+        int indexOfK = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == k) {
+                indexOfK = i;
                 break;
-        }
-        return index;
-    }
-    private static void pivotalPartitioning(int[] arr, int low, int high, int pivotal) {
-        int i = low - 1;
-        int firstMaxIndex = 0;
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivotal) {
-                swap(arr, ++i, j);
-            } else {
-
             }
         }
-        swap(arr, ++i, high - 1);
-    }
+        for (int i = 0; i < n; i++) {
+//            if (arr[i] > k && i < indexOfK) {
+//                continue;
+//            }
+            if (i > indexOfK && arr[i] <= arr[indexOfK]) {
+                int temp = arr[i];
+                for (int j = i; j >= indexOfK ; j--) {
+                    if (j == indexOfK) {
+                        arr[indexOfK] = temp;
+                        indexOfK = j + 1;
+                    } else {
+                        arr[j] = arr[j - 1];
+                    }
+                }
 
-    private static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+            } else if (i < indexOfK && arr[i] > arr[indexOfK]) {
+                for (int j = indexOfK; j >= i ; j--) {
+                    if (j == i) {
+                        arr[i] = k;
+                    } else {
+                        arr[j] = arr[j - 1];
+                    }
+                }
+                indexOfK = i;
+            }
+
+        }
+        return arr;
     }
 }
